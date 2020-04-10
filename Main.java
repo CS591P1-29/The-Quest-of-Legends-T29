5186dbbe2cb5375b82d49abf8d5291aa89291f93
bofeng <bournafe@gmail.com>
2020-04-10 17:35:02 -0400

    Version 1.2: Updated Main.java

--- Main.java
+++ Main.java
@@ -8,18 +8,26 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		
 		
+		System.out.println(ZshColor.ANSI_RED + " - Which game do you wanna play? Please input 1 or 2.");
+		System.out.println(ZshColor.ANSI_RED + " - 1. The Quest");
+		System.out.println(ZshColor.ANSI_RED + " - 2. The Quest Of Legends" + ZshColor.ANSI_RESET);
 		
+		String gameType = "";
 		
-		/*
-		TheQuestSimulation theQuest = new TheQuestSimulation(scan);
-		theQuest.initialize(); // Pre-process all data
-		theQuest.simulate();
-		*/
-		
-		
-		TheQuestOfLegendsSimulation theQuestOfLegends = new TheQuestOfLegendsSimulation(scan);
-		theQuestOfLegends.initialize(); // Pre-process all data
-		theQuestOfLegends.simulate();
+		do {
+			System.out.println(ZshColor.ANSI_RED + " - Would you like to buy/sell items?  Yes/No" + ZshColor.ANSI_RESET);
+			gameType = scan.nextLine();
+			if (gameType.compareTo("1") == 0) {
+				TheQuestSimulation theQuest = new TheQuestSimulation(scan);
+				theQuest.initialize(); // Pre-process all data
+				theQuest.simulate();
+			}
+			else if (gameType.compareTo("2") == 0) {
+				TheQuestOfLegendsSimulation theQuestOfLegends = new TheQuestOfLegendsSimulation(scan);
+				theQuestOfLegends.initialize(); // Pre-process all data
+				theQuestOfLegends.simulate();
+			}
+		} while (gameType.compareTo("1") != 0 && gameType.compareTo("2") != 0);
 		
 		scan.close();
 	}
